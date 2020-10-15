2297269e465f451e4b51863f36b055c6009be891
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-10-15 20:16:41 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,11 +9,12 @@ def second_challenge
       },
     :capulet => {
       #your key/value pairs here!
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-      }}
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
+        :hero_friends => []
+      }
+  }
 
 
 end
